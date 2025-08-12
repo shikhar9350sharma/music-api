2be3df3d478d258dd83698bbe12b0e43261792b9
shikhar9350sharma <shikharsharma593@gmail.com>
2025-08-12 23:44:58 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 const jsonServer = require('json-server');
+const path = require('path');
+
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(path.join(__dirname, '..', 'db.json'));
 const middlewares = jsonServer.defaults();
+
+server.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 server.use(middlewares);
 server.use(router);
