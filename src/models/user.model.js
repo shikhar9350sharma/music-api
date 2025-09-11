7045de83d4672b5b21f8df46ce92e7f7431f6b1f
shikhar9350sharma <shikharsharma593@gmail.com>
2025-09-11 11:03:41 +0530

    initial commit

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema(
         },
         username:{
             type:String,
-            require:true,
+            required:true,
             unique: true,
         },
         profilePic:{
